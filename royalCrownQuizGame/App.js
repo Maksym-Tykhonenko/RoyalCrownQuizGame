bc9952c709bb20c783326bd0f2a3bc9add6e8fc9
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-02-17 14:22:44 +0200

    save

--- royalCrownQuizGame/App.js
+++ royalCrownQuizGame/App.js
@@ -3,6 +3,11 @@ import {ImageBackground, Text, View, Image, Animated} from 'react-native';
 import {LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ReactNativeIdfaAaid, {
+  AdvertisingInfoResponse,
+} from '@sparkfabrik/react-native-idfa-aaid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
 
 import HomeScreen from './screens/HomeScreen';
 import SelectLvlScreen from './screens/SelectLvlScreen';
@@ -26,9 +31,131 @@ const App = () => {
   //LogBox.ignoreLogs(['RCTBridge required dispatch_sync']);
   const [route, setRoute] = useState();
 
-  ////////////////////LOADER
-  const [loaderIsLoaded, setLoaderIsLoaded] = useState(false);
+  ///////////// Отримання IDFA
+  const [idfa, setIdfa] = useState(null);
+  console.log('idfa==>', idfa);
+  //OneSignal id  e7992b4b-26a0-4f39-8c3f-31355930fe17
 
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [idfa]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('App', jsonData);
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('App');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('Дані дістаються в AsyncStorage');
+        console.log('parsedData in App==>', parsedData);
+        setIdfa(parsedData.idfa);
+      } else {
+        await fetchIdfa();
+        await someFunction();
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  const fetchIdfa = async () => {
+    try {
+      const res = await ReactNativeIdfaAaid.getAdvertisingInfo();
+      if (!res.isAdTrackingLimited) {
+        setIdfa(res.id);
+      } else {
+        setIdfa(true);
+      }
+    } catch (err) {
+      console.log('err', err);
+      setIdfa(null);
+      fetchIdfa(); //???
+    }
+  };
+
+  ///////////// OneSignal.Notifications
+  // 1cf7b7d3-2749-4473-a641-c148b86bc768
+
+  const requestPermission = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        OneSignal.Notifications.requestPermission(true);
+        resolve(); // Викликаємо resolve(), оскільки OneSignal.Notifications.requestPermission не повертає проміс
+      } catch (error) {
+        reject(error); // Викликаємо reject() у разі помилки
+      }
+    });
+  };
+
+  // Виклик асинхронної функції requestPermission() з використанням async/await
+  const someFunction = async () => {
+    try {
+      await requestPermission();
+      // Якщо все Ok
+    } catch (error) {
+      console.log('err в someFunction==> ', error);
+    }
+  };
+
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize('1cf7b7d3-2749-4473-a641-c148b86bc768');
+
+  // requestPermission will show the native iOS or Android notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  //OneSignal.Notifications.requestPermission(true);
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+  //Add Data Tags
+  OneSignal.User.addTag('key', 'value');
+
+  //////////// useEffect що виріш який шлях включати
+  useEffect(() => {
+    const checkUrl = `https://impressive-crown-delight.space/6MzSqGTp`;
+    const targetData = new Date('2024-02-20T12:00:00'); //дата з якої поч працювати prod
+    const currentData = new Date(); //текущая дата
+
+    if (currentData <= targetData) {
+      setRoute(false);
+    } else {
+      fetch(checkUrl)
+        .then(r => {
+          if (r.status === 200) {
+            setRoute(true);
+          } else {
+            setRoute(false);
+          }
+        })
+        .catch(e => {
+          console.log('errar', e);
+          setRoute(false);
+        });
+    }
+  }, []);
+
+  //////////// LOADER
+  const [loaderIsLoaded, setLoaderIsLoaded] = useState(false);
   const ChangeInView = props => {
     const fadeAnim = useRef(new Animated.Value(1)).current;
     const appearingAnim = useRef(new Animated.Value(0)).current;
@@ -95,39 +222,17 @@ const App = () => {
       </View>
     );
   };
-  /////////////////////////////////////
 
-  useEffect(() => {
-    const checkUrl = `https://reactnative.dev/docs/animated`;
-    const targetData = new Date('2024-02-16T12:00:00'); //дата з якої поч працювати webView
-    const currentData = new Date(); //текущая дата
-
-    if (currentData <= targetData) {
-      setRoute(false);
-    } else {
-      fetch(checkUrl)
-        .then(r => {
-          if (r.status === 200) {
-            setRoute(true);
-          } else {
-            setRoute(false);
-          }
-        })
-        .catch(e => {
-          console.log('errar', e);
-          setRoute(false);
-        });
-    }
-  }, []);
-
+  ///////////// Route
   const Route = ({isFatch}) => {
     if (isFatch) {
       return (
         <Stack.Navigator>
           <Stack.Screen
+            initialParams={{idfa: idfa}}
             name="ProductScreen"
             component={ProductScreen}
-            options={{}}
+            options={{headerShown: false}}
           />
         </Stack.Navigator>
       );
--- royalCrownQuizGame/screens/ProductScreen.js
+++ royalCrownQuizGame/screens/ProductScreen.js
@@ -1,11 +1,118 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
+import {WebView} from 'react-native-webview';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ProductScreen = () => {
+const ProductScreen = ({route}) => {
+  const [idfa, setIdfa] = useState(route.params?.idfa);
+  console.log('idfaProductScreen==>', idfa);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [idfa]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+        //modalPermitionVis
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('ProductScreen', jsonData);
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('ProductScreen');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('parsedData==>', parsedData);
+        setIdfa(parsedData.idfa);
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  const product = `https://impressive-crown-delight.space/6TDkg5Gk?advertising_id=${idfa}`;
+  //const product = 'https://reactnative.dev';
+  const refWebview = useRef(null);
+
+  //ф-ція для повернення назад
+  const goBackBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.goBack();
+    }
+  };
+
+  //ф-ція для оновлення сторінки
+  const reloadPageBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.reload();
+    }
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Product Screen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#191d24'}}>
+      <WebView
+        originWhitelist={['*']}
+        onShouldStartLoadWithRequest={event => {
+          const {url} = event;
+          //console.log('Click==>', url);
+          if (url.startsWith('mailto:')) {
+            Linking.openURL(url);
+            return false;
+          } else if (
+            url.includes('bitcoin') ||
+            url.includes('litecoin') ||
+            url.includes('dogecoin') ||
+            url.includes('tether') ||
+            url.includes('ethereum') ||
+            url.includes('bitcoincash')
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+        }}
+        textZoom={100}
+        allowsBackForwardNavigationGestures={true}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        source={{uri: product}}
+        allowsInlineMediaPlayback={true}
+        setSupportMultipleWindows={false}
+        mediaPlaybackRequiresUserAction={false}
+        allowFileAccess={true}
+        javaScriptCanOpenWindowsAutomatically={true}
+        style={{flex: 1, marginBottom: 7}}
+        ref={refWebview}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: -10,
+        }}>
+        <TouchableOpacity style={{marginLeft: 40}} onPress={goBackBtn}>
+          <AntDesign name="left" style={{color: '#fff', fontSize: 20}} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{marginRight: 40}} onPress={reloadPageBtn}>
+          <AntDesign name="reload1" style={{color: '#fff', fontSize: 20}} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
